4f724bc4687039a27d3289cc7bc8645ed813cffd
jwcomp4 <jwcompton4@gmail.com>
2022-09-03 21:39:36 -0500

    change color to correspond with magnitude and added popup, corrected logicStep2.js code.

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -65,7 +65,7 @@ d3.json(earthquake7).then(function(data){
     return {
       opacity: 1, 
       fillOpacity: 1, 
-      fillColor: "#ffae42", 
+      fillColor: getColor(feature.properties.mag), 
       color: "#000000",
       radius: getRadius(feature.properties.mag), 
       stroke: true,
@@ -78,12 +78,35 @@ d3.json(earthquake7).then(function(data){
       return magnitude * 4;
     }
   }
+  function getColor(magnitude) {
+    if (magnitude > 5) {
+      return "#ea2c2c";
+    }
+    if (magnitude > 4) {
+      return "#ea822c";
+    }
+    if (magnitude > 3) {
+      return "#ee9c00";
+    }
+    if (magnitude > 2) {
+      return "#eecc00";
+    }
+    if (magnitude > 1) {
+      return "#d4ee00";
+    }
+    return "#98ee00";
+  }
   L.geoJSON(data, {
     // Turn each feature into circlerMarker on the map:
     pointToLayer: function(feature, latlng){
       return L.circleMarker(latlng);
     }, 
-    style: styleInfo
+    style: styleInfo,
+    // Create a popup for each circle marker to display magnitude & location of the eq after the marker has been created
+    // and styled.
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
   }).addTo(map);
 });
 
